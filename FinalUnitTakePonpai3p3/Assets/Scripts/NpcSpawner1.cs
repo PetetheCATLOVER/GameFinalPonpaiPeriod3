1d43cd1c253e733e4f2f6a22d7f2931cfedd85e9
cogito17 <729426@student.dusd.net>
2025-12-12 22:15:14 -0800

    ...
    ...

---
+++ FinalUnitTakePonpai3p3/Assets/Scripts/NpcSpawner1.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class NpcSpawner1 : MonoBehaviour
+{
+    [Header("Npc Settings")]
+    public GameObject NpcPrefab;
+
+    [Header("Spawn Settings")]
+    public float spawnInterval = 2f;
+    public Transform spawnpoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnNpc), 0f, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnNpc()
+    {
+        Instantiate(NpcPrefab, spawnpoint.position, spawnpoint.rotation);
+    }
+}
